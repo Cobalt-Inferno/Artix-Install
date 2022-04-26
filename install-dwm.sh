537a252357b8b02ccabdbbfec094ae09a2974769
keroeslux <104093192+keroeslux@users.noreply.github.com>
2022-04-26 22:28:57 +0000

    Update install-dwm.sh

--- install-dwm.sh
+++ install-dwm.sh
@@ -4,10 +4,9 @@ echo -e "n\np\n2\n\n\nw" | fdisk /dev/vda
 mkfs.fat -F32 /dev/vda1
 mkfs.ext4 /dev/vda2
 mount /dev/vda2 /mnt
-cat << EOF | arch-chroot /mnt
+cat << EOF | artix-chroot /mnt
 basestrap /mnt base base-devel openrc elogind-openrc linux linux-firmware nano neofetch
-genfstab -U /mnt >> /mnt/etc/fstab
-timedatectl set-timezone Canada/Central
+fstabgen -U /mnt >> /mnt/etc/fstab
 locale-gen
 export LANG="en_US.UTF-8"
 export LC_COLLATE="C"
@@ -16,6 +15,9 @@ touch /etc/hosts
 echo "127.0.0.1     localhost" > /etc/hosts
 echo "::1           localhost" >> /etc/hosts
 echo "127.0.1.1     aslen-pc" >> /etc/hosts
+pacman -S dhclient connman-openrc connman-gtk --noconfirm
+echo 'hostname="aslen-pc"' > /etc/conf.d/hostname
+rc-update add connmand
 echo -e "test\ntest" | passwd root
 yes | pacman -S grub efibootmgr
 mkdir /boot/efi
